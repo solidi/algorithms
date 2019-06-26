98e94ca4e41ef2e4d3fea71d44e65a81a59b495b
Doug Arcuri <xsurrealx@hotmail.com>
2019-06-25 20:07:16 -0400

    Add tower of hanoi puzzle

---
+++ puzzles/TowerOfHanoi.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void towerOfHanoi(int n, char fromRod, char toRod, char auxRod) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from rod " + fromRod + " to rod " + toRod);
+            return;
+        }
+
+        towerOfHanoi(n - 1, fromRod, auxRod, toRod);
+        System.out.println("Move disk " + n + " from rod " + fromRod + " to rod " + toRod);
+        towerOfHanoi(n - 1, auxRod, toRod, fromRod);
+    }
+
+    public static void main(String args[]) {
+        towerOfHanoi(4, 'A', 'B', 'C');
+    }
+
+}
