2af833ab4dd7301f3ad996fb90b3c7771ceb1cb3
Doug Arcuri <xsurrealx@hotmail.com>
2019-06-29 10:04:18 -0400

    Add iterative approach to BinarySearch

--- search/BinarySearch.java
+++ search/BinarySearch.java
@@ -2,6 +2,26 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
+    public static int binarySearchIterative(int what[], int find) {
+        int startPoint = 0;
+        int endPoint = what.length;
+        int midPoint = (startPoint + endPoint) / 2;
+
+        while (startPoint <= endPoint) {
+            if (find > what[midPoint]) {
+                startPoint = midPoint + 1;
+            } else if (find < what[midPoint]) {
+                endPoint = midPoint - 1;
+            } else {
+                return what[midPoint];
+            }
+
+            midPoint = (startPoint + endPoint) / 2;
+        }
+
+        return -1;
+    }
+
     public static int binarySearch(int what[], int find, int startIndex, int endIndex) {
         int middleIndex = (startIndex + endIndex) / 2;
 
@@ -21,5 +41,6 @@ class Solution {
     public static void main(String args[]) {
         int whatToSearch[] = { 1, 5, 7, 9, 11, 15, 56, 90 };
         System.out.println(binarySearch(whatToSearch, 90, 0, whatToSearch.length));
+        System.out.println(binarySearchIterative(whatToSearch, 90));
     }
 }
