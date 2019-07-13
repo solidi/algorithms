b521b77376820e97208265d325f5cb8dddc515b5
Doug Arcuri <xsurrealx@hotmail.com>
2019-07-13 18:33:02 -0400

    Correct the bubble sort algo with generated values

--- sorts/BubbleSort.java
+++ sorts/BubbleSort.java
@@ -3,15 +3,14 @@ import java.util.*;
 
 /*
   Write a solution to the Bubble Sort.
- */
+*/
 class Solution {
-    // 3, 2, 1, 10, 5, 8, 2, 1
     public static int[] bubbleSort(int values[]) {
         for (int i = 0; i < values.length; i++) {
-            for (int j = i; j < values.length; j++) {
-                if (values[i] > values[j]) {
-                    int temp = values[i];
-                    values[i] = values[j];
+            for (int j = 0; j < values.length - i - 1; j++) {
+                if (values[j] > values[j + 1]) {
+                    int temp = values[j + 1];
+                    values[j + 1] = values[j];
                     values[j] = temp;
                 }
             }
@@ -19,8 +18,16 @@ class Solution {
         return values;
     }
 
+    public static int[] generateArray(int size) {
+        int array[] = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 90) + 10;
+        }
+        return array;
+    }
+
     public static void main(String args[]) {
-        int result[] = bubbleSort(new int[] { 3, 2, 1, 10, 5, 8, 2, 1 });
+        int result[] = bubbleSort(generateArray(20));
 
         for (int i = 0; i < result.length; i++) {
             System.out.print(result[i] + " ");
