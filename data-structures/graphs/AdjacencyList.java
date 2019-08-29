61cc985b1aff5a30f91540292abc95958b304aa6
Doug Arcuri <xsurrealx@hotmail.com>
2019-08-28 20:03:23 -0400

    Encapsulate graph functions in adjacency list

--- data-structures/graphs/AdjacencyList.java
+++ data-structures/graphs/AdjacencyList.java
@@ -7,8 +7,8 @@ import java.util.*;
 */
 class Solution {
     public static class Graph {
-        int V;
-        LinkedList<Integer> adjListArray[];
+        private int V;
+        private LinkedList<Integer> adjListArray[];
 
         @SuppressWarnings("unchecked")
         public Graph(int V) {
@@ -18,34 +18,34 @@ class Solution {
                 adjListArray[i] = new LinkedList<>();
             }
         }
-    }
 
-    public static void addEdge(Graph graph, int src, int dest) {
-        graph.adjListArray[src].add(dest);
-        graph.adjListArray[dest].add(src);
-    }
+        public void addEdge(int src, int dest) {
+            adjListArray[src].add(dest);
+            adjListArray[dest].add(src);
+        }
 
-    public static void printGraph(Graph graph) {
-        for (int v = 0; v < graph.V; v++) {
-            System.out.println("Adjacency list of vertex " + v);
-            System.out.print("head");
-            for (Integer pCrawl : graph.adjListArray[v]) {
-                System.out.print(" -> " + pCrawl);
+        public void printGraph() {
+            for (int v = 0; v < V; v++) {
+                System.out.println("Adjacency list of vertex " + v);
+                System.out.print("head");
+                for (Integer pCrawl : adjListArray[v]) {
+                    System.out.print(" -> " + pCrawl);
+                }
+                System.out.println("\n");
             }
-            System.out.println("\n");
         }
     }
 
     public static void main(String args[]) {
         int V = 5;
         Graph graph = new Graph(V);
-        addEdge(graph, 0, 1);
-        addEdge(graph, 0, 4);
-        addEdge(graph, 1, 2);
-        addEdge(graph, 1, 3);
-        addEdge(graph, 1, 4);
-        addEdge(graph, 2, 3);
-        addEdge(graph, 3, 4);
-        printGraph(graph);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 4);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 4);
+        graph.printGraph();
     }
 }
