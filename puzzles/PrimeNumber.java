5a14147ba9fad2d20e808a4b0565d6ed1f6800ea
Doug Arcuri <xsurrealx@hotmail.com>
2019-09-28 07:38:54 -0400

    Add additional isPrime implementations

--- puzzles/PrimeNumber.java
+++ puzzles/PrimeNumber.java
@@ -2,11 +2,32 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    public static boolean prime(int number) {
+    public static boolean isPrimeAll(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPrimeEvens(int number) {
+      for (int i = 2; i * 2 < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPrimeEfficient(int number) {
         if (number % 2 == 0) {
             return false;
         }
 
+        // then just check the odds.
         for (int i = 3; i * i <= number; i += 2) {
             if (number % i == 0) {
                 return false;
@@ -17,6 +38,8 @@ class Solution {
     }
 
     public static void main(String args[]) {
-        System.out.println("Is this number prime?: " + prime(13));
+        System.out.println("126 this number prime?: " + isPrimeEfficient(126));
+        System.out.println("127 this number prime?: " + isPrimeEfficient(127));
+        System.out.println("128 this number prime?: " + isPrimeEfficient(128));
     }
 }
